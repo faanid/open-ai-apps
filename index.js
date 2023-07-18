53218d7b659a2de4a1350b17b915b74f4ac3f7b3
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-07-18 12:55:47 +0430

    feat:init the boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+const setupTextarea = document.getElementById("setup-textarea");
+const setupInputContainer = document.getElementById("setup-input-container");
+const movieBossText = document.getElementById("movie-boss-text");
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+  }
+});
